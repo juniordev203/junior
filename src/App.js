7bda48bc175e9a9453f899f4e49f5b8c340a8f35
juniordev203 <ttk11092k3@gmail.com>
2024-06-07 02:31:48 +0700

    update css by junior

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import Home from "./components/home/home";
 import Projects from "./components/projects/projects";
 import Footer from "./components/footer";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 import ScrollToTop from "./components/scrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxCroxz2jYoKqNsq3Qm49am6rRZYwGPLc",
+  authDomain: "myportfolio-ca834.firebaseapp.com",
+  projectId: "myportfolio-ca834",
+  storageBucket: "myportfolio-ca834.appspot.com",
+  messagingSenderId: "979174821396",
+  appId: "1:979174821396:web:3f9340fbbe0979cdede101"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -33,7 +51,7 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
